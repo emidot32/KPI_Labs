9021645ecf17868db051484e8c051b1d9b7b2e3d
emidot32 <Harula74>
2021-09-19 13:23:24 +0300

    Delete .idea

--- Cybersecurity/lab8/dsa.py
+++ Cybersecurity/lab8/dsa.py
@@ -1,3 +1,5 @@
+import time
+
 from Crypto.PublicKey import DSA
 from Crypto.Signature import DSS
 from Crypto.Hash import SHA256
@@ -16,16 +18,19 @@ def generate_key():
 
 def sign_message(text):
     key = generate_key()
+    start_time = time.perf_counter_ns()
     text = text.encode('utf-8')
     hash_obj = SHA256.new(text)
     signer = DSS.new(key, 'fips-186-3')
     signature = signer.sign(hash_obj)
+    print("Час підпису: ", (time.perf_counter_ns() - start_time) / 1000)
     return signature
 
 
 def verify(signature):
     file = open("public_key.pem", "r")
     flag = False
+    start_time = time.perf_counter_ns()
     pub_key = DSA.import_key(file.read())
     verifier = DSS.new(pub_key, 'fips-186-3')
     for password in in_memory_password:
@@ -36,13 +41,14 @@ def verify(signature):
             break
         except ValueError:
             pass
+    print("Час верифікації: ", (time.perf_counter_ns() - start_time) / 1000)
     if flag:
         print("You are authenticated!")
     else:
         print("You are not authenticated!")
 
 
-msg = "pass3"
+msg = "pass3pass3pass3pass3pass3"
 signature = sign_message(msg)
 print(msg)
 print(signature)
